296fac742851195e74daed14356326a29df46d61
Robert <22194161+robert-leitl@users.noreply.github.com>
2022-10-28 18:01:41 +0200

    integrate cell based sph sim

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# ferrofluid
+# Ferrofluid
+
+![Ferrofluid Screenshot](https://github.com/robert-leitl/ferrofluid/blob/main/cover.jpg?raw=true)
+
+TBD
+
+[DEMO](https://robert-leitl.github.io/ferrofluid/dist/?debug=true)
+
+### Features
+- TBD
---
+++ src/app/shader/spikes.frag.glsl
@@ -0,0 +1,23 @@
+#version 300 es
+
+precision highp float;
+
+uniform sampler2D u_particlePosTexture;
+
+out vec4 outColor;
+
+in vec3 v_position;
+in vec2 v_texcoord;
+in vec3 v_normal;
+
+
+void main() {
+    ivec2 texSize = textureSize(u_particlePosTexture, 0);
+    int particleCount = texSize.x * texSize.y;
+
+    vec3 N = v_normal;
+    vec3 L = normalize(vec3(0., 1., -10.));
+    float dif = dot(L, N);
+
+    outColor = vec4(dif) * 0.8 + 0.2;
+}
